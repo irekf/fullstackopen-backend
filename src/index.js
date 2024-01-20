72a6d07d89aee607fe2faede122e81003eb73281
Irek Fakhrutdinov <irekfakh@gmail.com>
2024-01-20 20:00:04 +0100

    3.9 phonebook backend step9
    Signed-off-by: Irek Fakhrutdinov <irekfakh@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
 morgan.token('post_body', (req) => {
     return JSON.stringify(req.body)
@@ -10,6 +11,7 @@ app.use(morgan('tiny', {skip: (req, resp) => req.method === 'POST'}))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post_body',
     {skip: (req, res) => req.method !== 'POST'}))
 app.use(express.json())
+app.use(cors())
 
 let phonebook = [
     {
